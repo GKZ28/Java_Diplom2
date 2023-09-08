aa482751e0123530109bde3e7b038f577dffd9dc
Gamidov Magomed <maga.gamidov.98@mail.ru>
2023-09-08 11:44:41 +0300

    first commit

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -2,7 +2,6 @@ package ru.netology.javaqadiplom;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 public class SavingAccountTest {
 
     @Test
@@ -17,5 +16,53 @@ public class SavingAccountTest {
         account.add(3_000);
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+    }
+
+    @Test
+    public void testPay() {
+        SavingAccount account = new SavingAccount(2000, 1000, 10_000, 5);
+        account.pay(-1_000);
+
+        Assertions.assertEquals(2000 - 1000, account.getBalance());
+    }
+
+    @Test
+    public void testPayMoreThanMinBalance() {
+        SavingAccount account = new SavingAccount(2000, 1000, 10_000, 5);
+        account.pay(2000);
+
+        Assertions.assertEquals(1000, account.getMinBalance());
+    }
+
+    @Test
+    public void testAddZero() {
+        SavingAccount account = new SavingAccount(2000, 1000, 10_000, 5);
+        account.add(0);
+
+        Assertions.assertEquals(2000, account.getBalance());
+    }
+
+    @Test
+    public void testAddMoreThanMaxBalance() {
+        SavingAccount account = new SavingAccount(2000, 1000,10_000, 5);
+        account.add(11_000);
+
+        Assertions.assertEquals(10_000, account.getMaxBalance());
+    }
+
+    @Test
+    public void testYearChange() {
+        SavingAccount account = new SavingAccount(2000, 1000, 10_000, 5);
+        account.yearChange();
+
+        Assertions.assertEquals(100, account.yearChange());
+    }
+
+    @Test
+    public void testYearChangeWithNegativeBalance() {
+        SavingAccount account = new SavingAccount(-2000, 1000, 10_000, 5);
+        account.yearChange();
+
+        Assertions.assertEquals(0, account.yearChange());
     }
 }
